febb5a6c54fc0bd341c01ef000e3ad1eb729aaab
ccomsi92 <Kris@D-10-156-6-24.dhcp4.washington.edu>
2019-04-19 19:44:28 -0700

    functionality done

--- LineUp/Routing/views.py
+++ LineUp/Routing/views.py
@@ -1,10 +1,69 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+############################################################
+# Calendar API
+# from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+#############################################################
+
+
 def index(request): 
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+
+    # print (os.getcwd())
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+
+
+    destination = events_result['items'][0]['location']
+    #print(destination)
+    # exit()
+
+
+
+
+    
+    
+    
     content = {
-        'start': 'uw bothell', 
-        'end': 'seattle',
+        'origin': 'Seattle', 
+        'destination': destination,
     }
     # return HttpResponse("Hello World")
     
